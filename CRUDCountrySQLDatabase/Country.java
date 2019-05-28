d4e4454add9fb5d683a1eb9bbf2e4248a3edabaa
yandellbradley <50938551+yandellbradley@users.noreply.github.com>
2019-05-28 16:44:34 -0600

    Create Country.java

---
+++ CRUDCountrySQLDatabase/Country.java
@@ -0,0 +1,25 @@
+
+package countryapp;
+
+
+/**
+ *
+ * @author Bradley
+ */
+
+public class Country {
+    
+    private String countryName;
+   
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String name) {
+        this.countryName = countryName;
+    }
+
+    
+  }
+   
