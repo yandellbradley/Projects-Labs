6eff48df5a4667ef4b15c70cb7277b508bab11a9
yandellbradley <50938551+yandellbradley@users.noreply.github.com>
2019-05-28 13:17:14 -0600

    Create CustomerUTIL.java

---
+++ CRUDcustomerFromSQLdatabase/CustomerUTIL.java
@@ -0,0 +1,56 @@
+package customerapp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Bradley
+ */
+public class CustomerUTIL {
+    
+     private static Connection connection;
+    
+    private CustomerUTIL() {}
+
+    public static synchronized Connection getConnection() throws DBException {
+        if (connection != null) {
+            return connection;
+        }
+        else {
+            
+            try {
+                Class.forName("com.mysql.jbc.Driver");
+            } catch(ClassNotFoundException e) {
+               // System.out.println(e);
+            }
+            
+            try {
+                // set the db url, username, and password
+                String url = "jdbc:mysql://localhost:3306/mma";
+                String username = "mma_user";
+                String password = "sesame";
+
+                // get and return connection
+                connection = DriverManager.getConnection(url, username, password);
+                return connection;
+            } catch (SQLException e) {
+                throw new DBException(e);
+            }            
+        }
+    }
+    
+    public static synchronized void closeConnection() throws DBException {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new DBException(e);
+            } finally {
+                connection = null;                
+            }
+        }
+    }
+    
+}
